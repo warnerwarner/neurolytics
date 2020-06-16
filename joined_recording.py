f7b949028dcb11d5320fd39b2cbdae48949cf883
warnerwarner <tom.p.a.warner@gmail.com>
2020-06-16 15:16:22 +0100

    Add get_trial_response + saved index for bootstrap

--- joined_recording.py
+++ joined_recording.py
@@ -26,7 +26,7 @@ class JoinedRecording():
         trial_names = list(set(np.concatenate(trial_names)))
         self.trial_names = trial_names
 
-    def get_binned_trial_response(self, trial_name, *, pre_trial_window=None, post_trial_window=None, baselined=False, w_nans=False, w_bootstrap=True):
+    def get_binned_trial_response(self, trial_name, *, pre_trial_window=None, post_trial_window=None, baselined=False, w_nans=False, w_bootstrap=True, saved_trial=False):
         assert trial_name in self.trial_names, 'Trial is not in any recording'
         rec_responses = []
         for recording in self.recordings:
@@ -43,14 +43,14 @@ class JoinedRecording():
         max_reps = max(set([i.shape[0] for i in rec_responses]))
         repeat_lengths = [len(i) for i in rec_responses]
         if len(set(repeat_lengths)) != 1 and not w_bootstrap:
-            raise warnings.warn('Mismatch in repeat lengths, cannot make full numpy array')
+            warnings.warn('Mismatch in repeat lengths, cannot make full numpy array')
             mismatched_repeats = True
         else:
             mismatched_repeats = False
             w_nans = False
         if mismatched_repeats:
             if w_nans and w_bootstrap:
-                raise warnings.warn('Both nans and bootstrap selected, choosing bootstrap')
+                warnings.warn('Both nans and bootstrap selected, choosing bootstrap')
                 w_nans = False
 
         if w_nans:
@@ -75,22 +75,43 @@ class JoinedRecording():
 
         if w_bootstrap:
             bootstrap_size = max(repeat_lengths) * 2
+            if saved_trial:
+                bootstrap_size -= 2
             resampled_responses = []
+            all_saved_trials = []
             for rec in rec_responses:
+                if saved_trial:
+                    saved_index = np.random.randint(len(rec))
+                    saved_trial = rec[i]
+                    all_saved_trials.append(saved_trial)
+                    rec = np.array(rec)[np.arange(len(rec)) != saved_index]
                 resampled_rec = [rec[i] for i in np.random.randint(0, len(rec), size=bootstrap_size)]
                 resampled_responses.append(resampled_rec)
             rec_responses = np.concatenate(resampled_responses, axis=1)
 
-
         # Response returned is of a shape exps x repeats x units x time
+        if saved_trial:
+            return xs[:-1], rec_responses, all_saved_trials
         return xs[:-1], rec_responses
+    
 
 
     def get_multi_trial_response(self, trial_names, *, pre_trial_window=None, post_trial_window=None, baselined=False):
         assert all(i in self.trial_names for i in trial_names), "One or more trial names not present in any recording"
-        trial_response = [self.get_binned_trial_response(trial_name,
+        trial_response = [self.get_binned_trial_response(i,
                                                          pre_trial_window=pre_trial_window,
                                                          post_trial_window=post_trial_window,
                                                          baselined=baselined)[1] for i in trial_names]
 
         return trial_response
+
+
+    def get_trial_response(self, trial_name, pre_trial_window=0.5, post_trial_window=0.5):
+        assert trial_name in self.trial_names, 'Trial is not in any recording'
+        rec_responses = []
+        for recording in self.recordings:
+            #print(recording)
+            if trial_name in recording.get_unique_trial_names():
+                all_cluster_spikes = recording.get_all_trial_response(trial_name, pre_trial_window=pre_trial_window, post_trial_window=post_trial_window)
+                rec_responses.append(all_cluster_spikes)
+        return rec_responses
